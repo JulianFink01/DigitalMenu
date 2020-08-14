06c1a97aed973da4590b1912c45ffac1279c2646
Julian Fink <julianfink01@gmail.com>
2020-08-14 09:47:53 +0200

    db change

--- entities/variables.ini.php
+++ entities/variables.ini.php
@@ -2,14 +2,11 @@
 
 
 [Database]
-//host = sql7.freemysqlhosting.net
-//name = sql7358746
-//user = sql7358746
-//password =Ylj2WT31yN
-host = localhost
-name = DigitalMenu
-user = root
-password =
+host = sql7.freemysqlhosting.net
+name = sql7360320
+user = sql7360320
+password = FJH2qmWwsL
+
 [Verwaltung]
 username = test
 password = test
